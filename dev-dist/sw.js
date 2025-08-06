834ffc655350bd7d7b7e695b4e20920b42f04317
Justin Uminga <justinuminga@gmail.com>
2025-08-07 02:09:17 +0800

    title

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t1nmjk1vbro"
+    "revision": "0.nfpn0465q4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
